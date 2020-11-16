12f3446390332e8f82ecb77e04f63c03311ea1fa
Mattias Lasersköld <mattias.l.sk@gmail.com>
2020-11-16 21:54:38 +0100

    Find global config files

--- src/files/config.cpp
+++ src/files/config.cpp
@@ -1,9 +1,17 @@
 #include "files/config.h"
+#include <cstdlib>
 
 filesystem::path findConfig(filesystem::path file) {
     auto abs = filesystem::absolute(file);
     if (filesystem::exists(abs)) {
         return abs;
+    }
+
+    if (auto env = getenv("HOME")) {
+        auto fullPath = filesystem::path{env} / ".config/medit" / file;
+        if (filesystem::exists(fullPath)) {
+            return fullPath;
+        }
     }
 
     //! Handle if config is more difficult to locate
