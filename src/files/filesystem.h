d6147541376addb4b43eb13d15bb01212976fb5f
Mattias Lasersköld <mattias.l.sk@gmail.com>
2020-11-23 12:13:10 +0100

    Small filesystem.h fix

--- src/files/filesystem.h
+++ src/files/filesystem.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#ifdef USE_EXPERIMENTAL_FILESYSTEM
-
-#include <experimental/filesystem>
-
-namespace filesystem = std::experimental::filesystem;
-
-#else
+#if __has_include(<filesystem>)
 
 #include <filesystem>
 
 namespace filesystem = std::filesystem;
+
+#else
+
+#include <experimental/filesystem>
+
+namespace filesystem = std::experimental::filesystem;
 
 #endif
