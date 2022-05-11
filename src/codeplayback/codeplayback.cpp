95175e5f46fcbe5e5892d17c0a14283436678651
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-05-11 13:52:21 +0200

    More playback experiments

--- src/codeplayback/codeplayback.cpp
+++ src/codeplayback/codeplayback.cpp
@@ -3,6 +3,17 @@
 #include "text/cursorops.h"
 #include "views/editor.h"
 #include <iostream>
+#include <thread>
+
+using namespace std::literals;
+
+auto testText = R"_(
+
+int main() {
+    std::cout << "hello\n";
+}
+
+)_"sv;
 
 int main(int argc, char *argv[]) {
     auto screen = GuiScreen{};
@@ -21,12 +32,19 @@ int main(int argc, char *argv[]) {
 
     for (; c != Key::Quit; c = screen.getInput()) {
         if (c != Key::None && c != Key::Unknown) {
-            editor.draw(screen);
+            screen.cursorStyle(CursorStyle::Block);
 
-            for (auto c : "hello") {
+            for (auto c : testText) {
+                screen.cursorStyle(CursorStyle::Beam);
                 insert(c, buffer.end());
+                editor.draw(screen);
+                //                editor.bufferView().cursorPosition(buffer.end());
+                editor.cursor(buffer.end());
+                editor.updateCursor(screen);
+                screen.refresh();
+                std::this_thread::sleep_for(30ms);
             }
-
+            screen.cursorStyle(CursorStyle::Block);
             screen.refresh();
         }
     }
