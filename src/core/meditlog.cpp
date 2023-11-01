2bf6211ef86c6078dc66bb3a4f52b769e8ad6938
Mattias Lasersköld <mattias.l.sk@gmail.com>
2023-11-01 09:24:11 +0100

    Finally make something out of the log header i wrote some time ago

--- src/core/meditlog.cpp
+++ src/core/meditlog.cpp
@@ -1,4 +1,5 @@
 #include "meditlog.h"
+#include <mutex>
 #include <queue>
 #include <string>
 
@@ -7,7 +8,14 @@ namespace {
 class MeditLog {
 public:
     void log(std::string text) {
-        loggedText.push(std::move(text));
+        auto lock = std::unique_lock{_mutex};
+
+        if (!_callback) {
+            loggedText.push(std::move(text));
+            return;
+        }
+
+        _callback(text);
     }
 
     static MeditLog &instance() {
@@ -16,10 +24,37 @@ public:
     }
 
     std::queue<std::string> loggedText;
+
+    void subscribe(std::function<void(std::string_view)> callback) {
+        _callback = callback;
+
+        // Flush the cached text
+        while (!loggedText.empty()) {
+            auto lock = std::unique_lock{_mutex};
+
+            _callback(loggedText.front());
+            loggedText.pop();
+        }
+    }
+
+    void unsubscribe() {
+        _callback = {};
+    }
+
+    std::function<void(std::string_view)> _callback;
+    std::mutex _mutex;
 };
 
 } // namespace
 
 void impl::logInternal(std::string text) {
     MeditLog::instance().log(std::move(text));
+}
+
+void subscribeToLog(std::function<void(std::string_view)> callback) {
+    MeditLog::instance().subscribe(callback);
+}
+
+void unsubscribeToLog() {
+    MeditLog::instance().unsubscribe();
 }
